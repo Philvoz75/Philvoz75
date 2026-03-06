a16747937b4563acaf2576240d9287864adb3ae2
Philvoz75 <141204798+Philvoz75@users.noreply.github.com>
2026-03-05 22:54:23 -0700

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -1,24 +1,28 @@
+#usage python3 -i eth
+
 import optparse
 import re
 import subprocess
-
+#take 2 args, interface and mac address want to change
 def get_arguments():
+    
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interface', dest='interface', help='interface change its MAC address')
     parser.add_option('-m', '--mac', dest='new_mac', help='New mac address')
     (options, args) = parser.parse_args()
-
+    #check if args is valid
     if not options.interface:
         parser.error('[-] Please specify the interface you want to change its MAC address')
     elif not options.new_mac:
         parser.error('[-] Please specify the new mac address')
     return options
+#shut down interface in order to change mac
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address to {interface} to new MAC address: {new_mac}")
     subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ehter",new_mac])
+    subprocess.call(["ifconfig", interface, "hw", "ether",new_mac])
     subprocess.call(["ifconfig", interface, "up"])
-
+#use subprocess lib call if config to find current mac, using re module.
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
 
@@ -36,6 +40,7 @@ print(f"[+] Current MAC address: {current_mac}")
 change_mac(options.interface, options.new_mac)
 
 current_mac = get_current_mac(options.interface)
+#check if mac has changed or not
 if current_mac == current_mac:
     print("[+] Current MAC address is the changed")
 else:
